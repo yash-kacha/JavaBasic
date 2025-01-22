4bda7436c648a9e4e3a7a10dd7e43a5b0a7b966f
Yash Kacha <yashkacha2705@gmail.com>
2025-01-22 23:54:10 +0530

    Add files via upload

---
+++ loops/forloop.java
@@ -0,0 +1,17 @@
+package loops;
+import java.util.*;
+
+public class forloop {
+    
+    public static void main(String args[])
+    {
+        for(int i=0;i<=10;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print(j);
+            }System.out.println();
+        }
+    }
+
+}
